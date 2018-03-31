26a1e0757051bbcc983a48edbb7fc28f4bab7a1a
Alexander Goldbeck-Löwe <goldbeckloewe@me.com>
2018-03-31 22:25:11 +0200

    Initial Commit

---
+++ firstApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  firstApp
+//
+//  Created by A. Goldbeck-Löwe on 31.03.18.
+//  Copyright © 2018 Goldi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
